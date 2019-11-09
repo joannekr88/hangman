5f7a186f527151a6f7b2eb6912a2ab28e112b286
Johanna <johannaleekroesing015@yahoo.com>
2019-11-09 09:39:43 -0500

    Add files via upload

---
+++ challenges--7-8-9.py
@@ -0,0 +1,72 @@
+                                 #"""Chapter 7"""#
+
+""" #challenge# 1"""
+
+# my code
+
+"""did it myself again YES!!!!!"""
+
+drama = ["walking dead","Entourage","Sopranos","The vampire diaries"]
+
+for shows in drama:
+    print(shows)
+
+# teaches code
+
+shows = ["The Walking Dead", "Entourage", "The Sopranos", "The Vampire Diaries"]
+for show in shows:
+    print(show)
+
+
+"""# Challenge # 2"""
+
+# My code
+
+"""did it myself the fist time yeah!!!!!"""
+for i in range(25,51):
+    print(i)
+
+
+
+#teaches code
+
+
+for i in range(25,51):
+    print(i)
+
+
+"""--#--#-5-#--#-#Challenge 5#-#---#---###"""
+
+# my code
+
+list2 = [8, 19, 148, 4]
+list3 = [9, 1, 33, 83]
+multed = []
+
+for a in list2:
+    for b in list3:
+        multed.append(a * b)
+
+
+print(multed)
+
+#teach code
+
+list1 = [8, 19, 148, 4]
+list2 = [9, 1, 33, 83]
+list3 = []
+
+for i in list1:
+    for j in list2:
+        mult = i * j
+        list3.append(i * j)
+
+print(list3)
+
+
+
+
+
+
+
+
